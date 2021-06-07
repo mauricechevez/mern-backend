c0e7564db6d956bb403beb27bf2aeb813969a2f3
mchevez <mauricechevez@gmail.com>
2021-06-07 10:11:01 -0400

    User:add schema

--- models/User.js
+++ models/User.js
@@ -3,5 +3,31 @@ const { Schema } = mongoose;
 
 // User Schema
 
+const UserSchema = new Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  password:{
+    type:String,
+    required:true,
+    minlength:8
+  },
+  timesLoggedIn:{
+    type:Number,
+    default:0
+  },
+  date:{
+    type:String,
+    default:Date.now()
+  }
+})
+
+
 const User = mongoose.model('User', userSchema);
 module.exports = User
